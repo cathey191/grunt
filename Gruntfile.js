b8641f111eda10ec18bb14d3f68c30ac67e06993
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-14 22:01:07 +0000

    setup linting and sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,74 @@ module.exports = function(grunt) {
 					}
 				]
 			}
+		},
+		uglify: {
+			my_target: {
+				files: {
+					'js/script.min.js': ['js/*.js']
+				}
+			}
+		},
+		csslint: {
+			strict: {
+				options: {
+					import: 2
+				},
+				src: ['css/*.css']
+			},
+			lax: {
+				options: {
+					import: false
+				},
+				cwd: 'css/',
+				src: ['css/*.css']
+			}
+		},
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				eqnull: true,
+				browser: true,
+				globals: {
+					jQuery: true
+				}
+			},
+			all: ['Gruntfile.js', 'js/*.js']
+		},
+		sass: {
+			dist: {
+				options: {
+					style: 'expand'
+				},
+				files: {
+					'css/style.css': 'sass/style.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['css/style.css', 'sass/style.scss'],
+				tasks: ['sass', 'csslint', 'cssmin']
+			},
+			js: {
+				files: ['js/script.js'],
+				tasks: ['jshint', 'uglify']
+			}
 		}
 	});
 
 	// grunt.loadNpmTasks();
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
 	// grunt.registerTask();
+	grunt.registerTask('default', ['sass', 'cssmin', 'csslint']);
+	grunt.registerTask('min', ['cssmin', 'uglify']);
+	grunt.registerTask('lint', ['sass', 'csslint', 'jshint']);
+	grunt.registerTask('w', ['watch']);
 };
